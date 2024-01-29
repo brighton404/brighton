125c67fb1e2b7e4c7e3d6a3d3c733addcd3a5a5d
Julius Brighton <brighton404@users.noreply.github.com>
2024-01-29 21:31:39 +0300

    1.4.6
    linked figma page and added a loader

--- public/pages/scripts/main/path.js
+++ public/pages/scripts/main/path.js
@@ -3,14 +3,15 @@ let currentPage = sessionStorage.getItem('currentPage') || 'profile';
 
 function loadProfile() {
     const about = document.getElementById('about');
-    const home = document.getElementById('home');
     const profile = document.getElementById('profile');
     const navhome = document.getElementById('navhome');
     const navabout = document.getElementById('navabout');
+    const FigmaLink = document.getElementById('Figma');
 
     profile.style.display = 'block';
     about.style.display = 'none';
     navhome.classList.add ('active');
+    FigmaLink.classList.remove ('active')
     navhome.style.display = 'none';
     navabout.style.display = 'block';
     
@@ -27,14 +28,15 @@ function loadProfile() {
 
 function loadAbout() {
     const about = document.getElementById('about');
-    const home = document.getElementById('home');
     const profile = document.getElementById('profile');
     const navabout = document.getElementById('navabout');
     const navhome = document.getElementById('navhome');
+    const FigmaLink = document.getElementById('Figma');
 
     profile.style.display = 'none';
     about.style.display = 'block';
     navabout.classList.add ('active');
+    FigmaLink.classList.remove('active')
     navabout.style.display = 'none';
     navhome.style.display = 'block';
 
@@ -60,14 +62,30 @@ document.addEventListener('DOMContentLoaded', function () {
 function toggleTruncation(element) {
     element.classList.toggle('expand-text');
 
-    const allElements = document.querySelectorAll('.with-x-years');
+    const allElements = document.querySelectorAll('.card-context');
     allElements.forEach((el) => {
       if (el !== element) {
         el.classList.remove('expand-text');
       }
     });
-  }
+}
 
-document.getElementById('Figma').addEventListener('click', function() {
-  window.location.href = 'https://www.figma.com/@brighton404'; 
+function Figma() {
+    const about = document.getElementById('about');
+    const profile = document.getElementById('profile');
+    const FigmaLink = document.getElementById('Figma');
+
+    profile.style.display.remove = 'active';
+    about.style.display.remove = 'active';
+    FigmaLink.classList.add ('active');
+
+    window.location.href = 'https://www.figma.com/@brighton404'; 
+}
+
+//loader
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('.loader-wrapper').style.display = 'none';
+});
+window.addEventListener('beforeunload', function () {
+    document.querySelector('.loader-wrapper').style.display = 'flex';
 });
